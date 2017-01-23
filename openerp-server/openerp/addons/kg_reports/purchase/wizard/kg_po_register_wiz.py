fe43c52feb1947851867da4d1447f527b9da5184
DineshKumar <dinesh@dinesh-Lenovo-H50-50.(none)>
2017-01-23 16:12:52 +0530

    DB password changed in jasper_report.py

--- openerp-server/openerp/addons/kg_reports/purchase/wizard/kg_po_register_wiz.py
+++ openerp-server/openerp/addons/kg_reports/purchase/wizard/kg_po_register_wiz.py
@@ -68,7 +68,7 @@ class kg_po_register_wiz(osv.osv_memory):
 		print data,' create_report('
 		return {
 			'type'		 : 'ir.actions.report.xml',
-			'report_name'   : 'jasper_kg_po_register_report',
+			'report_name'   : 'jasper_kg_po_register_report1',
 			'datas': {
 					'model':'kg.po.register.wiz',
 					'id': context.get('active_ids') and context.get('active_ids')[0] or False,
